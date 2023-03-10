4e003dcb3682ebfac458a4a9d4af7487e0253951
mrbunyod <bunyodlomg@gmail.com>
2023-03-10 18:29:01 +0500

    debuging

--- src/js/dropDown.js
+++ src/js/dropDown.js
@@ -2,18 +2,23 @@ const f1 = document.querySelector('.f1')
 const f2 = document.querySelector('.f2')
 const f3 = document.querySelector('.f3')
 const text = document.querySelector('.dropdown__text')
-
+let font = localStorage.getItem('font')
+document.body.style.fontFamily = font
 f1.addEventListener('click', () => {
     document.body.style.fontFamily = 'sans-serif'
     text.textContent = 'Sans-Serif'
+    localStorage.setItem('font', 'Sans-Serif')
+
 })
 f2.addEventListener('click', () => {
     document.body.style.fontFamily = 'serif'
     text.textContent = 'Serif'
+    localStorage.setItem('font', 'serif')
 })
 f3.addEventListener('click', () => {
     document.body.style.fontFamily = 'monospace'
     text.textContent = 'Mono'
+    localStorage.setItem('font', 'monospace')
 })
 
 f1.style.fontFamily = 'sans-serif'
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "production", // production
+    mode: "developer", // production
     entry: {
         app: path.resolve(__dirname, "./src/js/app.js"),
         // about: path.resolve(__dirname, "./src/js/about.js"),
